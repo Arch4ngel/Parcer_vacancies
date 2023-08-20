8488c69927040bbbf439f9860315a918b4441b47
Mikhail Gribkov <mishacool4@gmail.com>
2023-08-20 17:52:50 +0300

    done API classes and methods

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,2 +1,6 @@
 class Vacancy:
-    pass
+    def __init__(self, name, url, salary, requirements):
+        self.name = name
+        self.salary = salary
+        self.url = url
+        self.requirements = requirements
