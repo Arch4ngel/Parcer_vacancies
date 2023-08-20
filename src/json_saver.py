8208544b4e572161ab58e1c4df39891d895f1180
Mikhail Gribkov <mishacool4@gmail.com>
2023-08-20 13:26:56 +0300

    base structure

---
+++ src/json_saver.py
@@ -0,0 +1,12 @@
+class JSONSaver:
+    def __init__(self):
+        pass
+
+    def add_vacancy(self):
+        pass
+
+    def get_vacancies_by_salary(self):
+        pass
+
+    def delete_vacancy(self):
+        pass
